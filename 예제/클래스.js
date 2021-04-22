b09564f58f801d835caf26173f2f52d52374a49c
LimHyeonSu <dlagustn720@naver.com>
2021-04-22 23:05:39 +0900

    클래스_메서드

--- 예제/클래스.js
+++ 예제/클래스.js
@@ -24,3 +24,28 @@ const Person2 = class MyClass{};
 const me2 = new Person2();
 console.log(MyClass);  //error
 const you = new MyClass();  //error
+
+
+//5. 메서드
+class Person3 {
+    //생성자
+    constructor(name){
+        this.name = name;
+    }
+
+    //정적 메서드
+    static sayHi(){
+        console.log('Hi');
+    }
+} 
+
+Person3.sayHi();  //정적 메서드는 인스턴스로 호출하지 않고 클래스로 호출한다.
+const me = new Person3('Lee');
+me.sayHi();  //error 정적메서드는 인스턴스로 호출 불가
+
+//표준 빌트인 객체의 정적 메서드
+Math.max(1,2,3);
+Number.isNaN(NaN);
+JSON.stringify({a:1});
+Object.is({},{});
+Reflect.has({a:1},'a');
