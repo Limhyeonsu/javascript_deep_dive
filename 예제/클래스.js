c698b3d277398f5c2514765ce0762f989da0218d
LimHyeonSu <dlagustn720@naver.com>
2021-04-24 23:14:55 +0900

    클래스의 확장

--- 예제/클래스.js
+++ 예제/클래스.js
@@ -123,3 +123,118 @@ class MyMath {
 
 console.log(MyMath.PI);  //3.14285714..
 console.log(MyMath.increment());  //11
+
+
+//8. 상속에 의한 클래스 확장
+//클래스 상속
+class Animal {
+    constructor(age, weight){
+        this.age = age;
+        this.weight = weight;
+    }
+
+    eat() { return 'eat'; }
+    move() { return 'move'; }
+}
+
+class Bird extends Animal {
+    fly() { return 'fly'; }
+
+}
+const bird = new Bird(1, 5);
+
+console.log(bird);  //Bird {age:1, weight: 5}
+console.log(bird instanceof Bird);  //true
+console.log(bird instanceof Animal);  //true
+
+console.log(bird.eat());  //eat
+console.log(bird.move());  //movw
+console.log(bird.fly());  //fly
+
+//동적 상속
+//1)
+function Base(a){
+    this.a = a;
+}
+//생성자 함수를 상속받는 서브클래스
+class Derived extends Base {}
+
+const derived = new Derived(1);
+console.log(derived); //Derived {a:1}
+
+//2)
+function Base1() {}
+class Base2 {}
+
+let condition = true;
+//조건에 따라 상속 대상을 동적으로 결정
+class Derived2 extends (condition ? Base1 : Base2){}
+const derived2 = new Derived2;
+console.log(derived2); // Derived2 {}
+console.log(derived2 instanceof Base1);  //true
+console.log(derived2 instanceof Base2);  //false
+
+//서브 클래스의 constructor
+class Base3 {
+    constructor() {}  //constructor를 생략하면 암묵적으로 생성됨
+}
+
+class Derived3 extends Base3 {
+    constructor(...args) { super(...args); } //constructor 생략시 암묵적으로 생성
+}
+
+const derived3 = new Derived3();
+console.log(derived3);
+
+//상속 클래스의 인스턴스 생성과정
+//수퍼클래스
+class Rectangle { 
+    constructor(width, height) {
+        this.weight = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+    toString() {
+        return `width = ${this.width}, height = ${this.height}`;
+    }
+}
+
+//서브 클래스
+class ColorRectangle extends Rectangle {
+    constructor(width, height, color){
+        super(width, height);
+        this.color = color;
+    }
+    //메서드 오버라이딩
+    toString() {
+        return super.toString() + `, color = ${this.color}`;
+    }
+}
+
+const ColorRectangle = new ColorRectangle(2, 4, 'red');
+console.log(ColorRectangle);
+
+console.log(ColorRectangle.getArea());  //8
+console.log(ColorRectangle.toString());  //width = 2, height = 4, color = red
+
+//표준빌트인 생성자함수 확장
+//Array생성자 함수를 상속받은 클래스는 Array.prototype과 MyArray.prototype의 모든 메서드를 사용할 수 있다.
+class MyArray extends Array {  
+
+    //static get [Symbol.species]() {return Array;} 모든 메서드가 Array타입의 인스턴스를 반환 -> MyArray의 메서드와 메서드 체이닝이 불가능하다
+    uniq() {
+        return this.filter((v, i, self) => self.indexOf(v) === i);
+    }
+
+    avarage() {
+        return this.reduce((pre, cur) => pre + cur, 0) / this.length;
+    }
+}
+
+const myArray = new MyArray(1,1,2,3);
+console.log(myArray);  //MyArray(4) [1,1,2,3]
+
+console.log(myArray.uniq());  //MyArray(3) [1,2,3]
+console.log(myArray.avarage()); //1.75
